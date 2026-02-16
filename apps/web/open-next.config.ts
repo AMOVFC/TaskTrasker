1fd2e57313f5a501011ee3b74d7c4f8b8966787d
Andre <acardoso.ny@gmail.com>
2026-02-16 17:58:14 -0500

    update

--- apps/web/open-next.config.ts
+++ apps/web/open-next.config.ts
@@ -4,6 +4,8 @@
  * Keeping this file in-repo prevents the OpenNext CLI from prompting to
  * scaffold one interactively during CI builds.
  */
-const config = {};
+const config = {
+  default: {},
+};
 
 export default config;
